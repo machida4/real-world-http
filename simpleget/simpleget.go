790a796919a742e9e69b2844d5d941242c29a2a0
machida <machida@kmc.gr.jp>
2022-01-11 19:40:03 +0900

    フォルダ分け

--- simpleget/simpleget.go
+++ simpleget/simpleget.go
@@ -16,5 +16,10 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	// 文字列で "200 OK"
+	log.Println("Status:", resp.Status)
+	// 数値で
+	log.Println("StatusCode:", resp.StatusCode)
+
 	log.Println(string(body))
 }

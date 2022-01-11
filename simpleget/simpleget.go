6635b45b3ccdf854db6df4cbf5be710aad9c72ff
machida <machida@kmc.gr.jp>
2022-01-11 20:20:36 +0900

    post_multipart

--- simpleget/simpleget.go
+++ simpleget/simpleget.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"bytes"
+	"io"
 	"io/ioutil"
 	"log"
+	"mime/multipart"
 	"net/http"
+	"net/textproto"
 	"net/url"
+	"os"
 )
+
+func main() {
+	post_multipart()
+}
 
 func get() {
 	values := url.Values{
@@ -35,4 +44,57 @@ func head() {
 	}
 	log.Println("Status:", resp.Status)
 	log.Println("Headers:", resp.Header)
+}
+
+// x-www-form-urlencoded形式のPOST
+func post_urlencoded() {
+	values := url.Values{
+		"test": {"value"},
+	}
+
+	resp, err := http.PostForm("http://localhost:18888", values)
+	if err != nil {
+		panic(err)
+	}
+	log.Println("Status:", resp.Status)
+}
+
+// 任意のボディを送信
+func post_file() {
+	file, err := os.Open("simpleget/simpleget.go")
+	if err != nil {
+		panic(err)
+	}
+	resp, err := http.Post("http://localhost:18888", "text/plain", file)
+	if err != nil {
+		panic(err)
+	}
+	log.Println("Status:", resp.Status)
+}
+
+// multipart/form-data形式のPOST
+func post_multipart() {
+	var buffer bytes.Buffer
+	writer := multipart.NewWriter(&buffer)
+	writer.WriteField("name", "Michael Jackson")
+	part := make(textproto.MIMEHeader)
+	part.Set("Content-Type", "Image/jpeg")
+	part.Set("Content-Disposition", `form-data; name="thumbnail"; filename="photo.jpg`)
+	fileWriter, err := writer.CreatePart(part)
+	if err != nil {
+		panic(err)
+	}
+	readFile, err := os.Open("simpleget/photo.jpg")
+	if err != nil {
+		panic(err)
+	}
+	defer readFile.Close()
+	io.Copy(fileWriter, readFile)
+	writer.Close()
+
+	resp, err := http.Post("http://localhost:18888", writer.FormDataContentType(), &buffer)
+	if err != nil {
+		panic(err)
+	}
+	log.Println("Status:", resp.Status)
 }
